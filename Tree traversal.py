39981bfcf170d0074a0e6aef3c3a7b8ff5725255
sameer ranjan <40149707+wilfredarin@users.noreply.github.com>
2018-06-15 20:50:11 +0530

    Create Tree traversal.py

---
+++ Tree traversal.py
@@ -0,0 +1,19 @@
+'''Trees are non-linear data structure
+they can be transversed in many ways:
+
+   
+   
+   A.Depth first Traversal
+        1.Inorder(left root right)
+        2. Preorder(root left right )
+        3. Postorder(left right root)
+    
+    B. Breadth first Traversal:
+        
+        
+    
+        
+        
+
+
+'''
