9e4068f87e4e272a5d8e163a0699f1dbcdc77a84
sameer ranjan <40149707+wilfredarin@users.noreply.github.com>
2018-12-27 08:24:54 +0530

    DFS | inorder | preorder | postorder
    DFS tree traversal python3 implementation | inorder | preorder | postorder

--- Tree/Tree traversal.py
+++ Tree/Tree traversal.py
@@ -17,3 +17,44 @@ they can be transversed in many ways:
 
 
 '''
+class Node:
+	def __init__(self,data):
+			self.data = data 
+			self.left = None
+			self.right = None
+
+def inorder(root):
+	if root is not None:
+		inorder(root.left)
+		print(root.data)
+		inorder(root.right)
+
+def postorder(root):
+	if root is not None:
+		postorder(root.left)
+		postorder(root.right)
+		print(root.data)
+
+def preorder(root):
+	if root is not None:
+		print(root.data)
+		preorder(root.left)
+		preorder(root.right)
+
+
+
+root=Node(1)
+a=Node(2)
+b=Node(3)
+c=Node(4)
+d=Node(5)
+root.left=a
+root.right=b
+a.left=c
+a.right=d
+print("pre")
+preorder(root)
+print("post")
+postorder(root)
+print("inorder")
+inorder(root)
